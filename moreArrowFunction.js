50820c752e57acf5cb604e0cb269bccb7bce670b
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-02-01 23:55:01 +0600

    more arrow function

---
+++ moreArrowFunction.js
@@ -0,0 +1,22 @@
+//500 tk er 20% discount
+//20/100*500
+
+const getTex = (ammount,taxRate) => ammount*taxRate/100;
+
+//single parametters
+const square = (x) => x*x;
+console.log(square(7));
+//49
+
+const firstElement = nums =>nums[0];
+console.log(firstElement([30,32,40,45]))
+//output : 30
+
+const logit=()=>console.log(34)
+logit()
+//output : 34
+
+////// annymous function
+document.getElementById('').addEventListener('click',()=>{
+    
+})
