458ff78c93c9d3ea94f74bde910b62e8f9003710
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-02-01 22:48:54 +0600

    one argument and two parametter

--- default-parametter.js
+++ default-parametter.js
@@ -4,3 +4,19 @@ function sum (num1,num2){
 }
 
 sum(10,20)
+//output : 30
+
+
+function sub(num1,num2){
+    const sub = num1 - num2;
+    console.log(sub)
+}
+sub(20,10) //argument 20,10
+//output : 10
+
+function sum1(num1,num2){
+    const number = num1+num2;
+    console.log(num1,num2,number)
+}
+sum1(10)
+//output 10 undefined NaN
